a03b28ccfb049d2dee3743bc60b6a3c8f2f4d5bb
anyelinash <anyelina.sosa@tecsup.edu.pe>
2024-05-06 13:58:03 -0500

    Initial Commit

---
+++ SosaColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SosaColeccionDeJuegos
+//
+//  Created by Anyelina Sosa Hualpa on 6/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
